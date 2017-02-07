edf7f92bf409d2ba971e57ea2ccf32030fe8de6e
jkirkpatrick <josh_kirkpatrick@hotmail.com>
2017-02-07 16:06:21 +0000

    l

--- Creater.py
+++ Creater.py
@@ -3,7 +3,7 @@ import pyqrcode
 
 def create(filename = "Test.png", data = "", error='H', scale = 6):
     Code = pyqrcode.create(data,error)
-    Code.png(filename, scale)
+    Code.png(filename + ".png", scale)
     return True
 
 data = {"PENNINEMANUFACTURING":{"ProductCode":"XASD2", "Quantity":"123"}}
