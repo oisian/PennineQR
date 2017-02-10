10b5bf9d200eeaf7e71ca0d892bbc2d6e926a85d
jkirkpatrick <josh_kirkpatrick@hotmail.com>
2017-02-10 15:46:45 +0000

    l

--- realtime.py
+++ realtime.py
@@ -1,50 +1,20 @@
-import zbar
-
-from PIL import Image
+import numpy as np
 import cv2
 
+cap = cv2.VideoCapture(0)
 
-def main():
-    """
-    A simple function that captures webcam video utilizing OpenCV. The video is then broken down into frames which
-    are constantly displayed. The frame is then converted to grayscale for better contrast. Afterwards, the image
-    is transformed into a numpy array using PIL. This is needed to create zbar image. This zbar image is then scanned
-    utilizing zbar's image scanner and will then print the decodeed message of any QR or bar code. To quit the program,
-    press "q".
-    :return:
-    """
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-    # Begin capturing video. You can modify what video source to use with VideoCapture's argument. It's currently set
-    # to be your webcam.
-    capture = cv2.VideoCapture(0)
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    while True:
-        # To quit this program press q.
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        # Breaks down the video into frames
-        ret, frame = capture.read()
-
-        # Displays the current frame
-        cv2.imshow('Current', frame)
-
-        # Converts image to grayscale.
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Uses PIL to convert the grayscale image into a ndary array that ZBar can understand.
-        image = Image.fromarray(gray)
-        width, height = image.size
-        #zbar_image = zbar.Image(width, height, 'Y800', image.tostring())
-
-        # Scans the zbar image.
-        #scanner = zbar.ImageScanner()
-        #scanner.scan(zbar_image)
-
-        # Prints data from image.
-        #for decoded in zbar_image:
-        #   print(decoded.data)
-
-
-if __name__ == "__main__":
-    main()
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
