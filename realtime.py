87f73c791f95757f6a679afa243eb1d6869738ee
jkirkpatrick <josh_kirkpatrick@hotmail.com>
2017-02-10 16:10:28 +0000

    l

--- realtime.py
+++ realtime.py
@@ -1,20 +1,16 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('output.avi')
+ret, frame = cap.read()
 
-while(True):
-    # Capture frame-by-frame
+while(ret):
     ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('outVideo',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    ret, frame = cap.read()
 
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
