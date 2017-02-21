7f79e5e19884c4f0e7577661f8637eb277bb14d4
jkirkpatrick <josh_kirkpatrick@hotmail.com>
2017-02-21 10:00:30 +0000

    l

--- realtime.py
+++ realtime.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 def detect(image):
     # convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -36,34 +37,44 @@ def detect(image):
     # otherwise, sort the contours by area and compute the rotated
     # bounding box of the largest contour
     c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+
+    # Draw box on screen
     rect = cv2.minAreaRect(c)
     box = cv2.boxPoints(rect)
     box = np.int0(box)
-    r = cv2.boundingRect(box)
+    cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
+
+    x, y, width, height = cv2.boundingRect(c)
+    x = x - 30
+    y = y - 30
+    roi = frame[y:y + height + 50, x:x + width + 50]
+    dst = cv2.resize(roi, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    return dst
+    # cv2.imwrite("roi.png", dst)
+
+
+
+
 
     # return the bounding box of the barcode
-    return box
+    # return box
+
 
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
+if vc.isOpened():  # try to get the first frame
     rval, frame = vc.read()
 else:
     rval = False
 
 while rval:
     box = detect(frame)
-    cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
     cv2.imshow("preview", frame)
-
-    #crop_img = frame[box[0]:box[1], box[2]:box[3]]  # Crop from x, y, w, h -> 100, 200, 300, 400
-    # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
-    #cv2.imshow("cropped", crop_img)
-
+    cv2.imwrite("roi.png", box)
     print("\n\n\n\n\n")
-    #cv2.imwrite('Barcode.png', img)
+    # cv2.imwrite('Barcode.png', img)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
+    if key == 27:  # exit on ESC
         break
