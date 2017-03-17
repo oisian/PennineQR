ac41899ecdee76b70d90ad1e159a0f681eec5cf5
jkirkpatrick <josh_kirkpatrick@hotmail.com>
2017-03-17 10:32:56 +0000

    l

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 import zxing
-# import zbarlight
+import zbarlight
 from  PIL import Image
 import Stream
 from connect import main
@@ -83,12 +83,13 @@ class StockMovement:
         #flag to wait for QR to pass
         self.hold = False
 
+        self.detect_mode = "Zbar";
+
     def start(self):
         self.poster = main.connection().start()
         self.stream = Stream.WebcamVideoStream(src=0).start()
         self.detector = realtime.CodeDetection().start()
         self.ip = self.poster.ip
-
         Thread(target=self.loop, args=()).start()
         return self
 
@@ -103,30 +104,56 @@ class StockMovement:
                 cv2.imshow("Unprocessed Frame", self.frame)
 
             if not self.hold:
-                img = self.detector.detect(self.frame)
+                img =  self.detector.detect(self.frame)
                 if img != None:
                     if self.show_image:
                         cv2.imshow("Frame", img)
-                    cv2.imwrite("roi.png", img)
-                    reader = zxing.BarCodeReader("C:/zxing/")
-                    barcode = reader.decode("roi.png")
-                    if barcode != None:
-                        data = barcode.data
-                        splitData = data.split(",")
-                        if splitData[0] == "pm":
-                            UNID = splitData[1].split(":")[1].rstrip()
-                            productCode = splitData[2].split(":")[1].rstrip()
-                            Quantity = splitData[3].split(":")[1].rstrip()
-                            if productCode and Quantity:
-                                self.poster.requests.append(
-                                    {"IP": self.ip, "UNID": UNID, 'ProductCode': productCode, 'Quantity': Quantity})
-                                if self.ip == self.ip1:
-                                    self.ip = self.poster.ip
-                                else:
-                                    self.ip = self.ip1
-                        self.hold = False
+
+                if self.detect_mode == "Zxing":
+                    self.xing_detext(img)
+                    self.hold = False
+
+                if self.detect_mode == "Zbar":
+                    self.xing_detext(img)
+                    self.hold = False
             else:
                 self.hold_check()
+
+            key = cv2.waitKey(20)
+            if key == 27:  # exit on ESC
+                break
+
+
+    def zbar_detect(self):
+        file_path = 'roi.png'
+        with open(file_path, 'rb') as image_file:
+            image = Image.open(image_file)
+            image.load()
+
+        codes = zbarlight.scan_codes('qrcode', image)
+        print('QR codes: %s' % codes)
+        return codes
+
+
+    def zxing_detext(self,img):
+        cv2.imwrite("roi.png", img)
+        reader = zxing.BarCodeReader("C:/zxing/")
+        barcode = reader.decode("roi.png")
+        if barcode != None:
+            data = barcode.data
+            splitData = data.split(",")
+            if splitData[0] == "pm":
+                UNID = splitData[1].split(":")[1].rstrip()
+                productCode = splitData[2].split(":")[1].rstrip()
+                Quantity = splitData[3].split(":")[1].rstrip()
+                if productCode and Quantity:
+                    self.poster.requests.append(
+                        {"IP": self.ip, "UNID": UNID, 'ProductCode': productCode, 'Quantity': Quantity})
+                    if self.ip == self.ip1:
+                        self.ip = self.poster.ip
+                    else:
+                        self.ip = self.ip1
+
 
     def hold_check(self):
         pass
@@ -139,4 +166,4 @@ class StockMovement:
         self.poster.stop()
 
 
-sm = StockMovement(show_image=False).start()
+sm = StockMovement(show_image=True).start()
